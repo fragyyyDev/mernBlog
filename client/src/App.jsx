d319a35e57c79dd8791340c3373744a572769c94
fragyyyDev <fragyyydev@gmail.com>
2024-09-20 09:14:42 +0200

    Created Pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route, createBrowserRouter } from 'react-router-dom'
 import './index.css'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Projects from './pages/Projects'
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
 
   return (
-    <>
-      <p className=''>OIOIOI BAAAKA</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
