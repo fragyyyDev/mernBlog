ccc5ec6e4006cdf5add8b47d9f7c84df423ca0a3
fragyyyDeveloper <fragyyyDev@gmail.com>
2024-09-22 14:03:04 +0200

    created signup api route, tested, makes users in MongoDB

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+import User from "../models/user.model.js"
+import bcryptjs from "bcryptjs"
+
+export const signup = async (req, res) => {
+    const { username , email , password} = req.body
+
+    if(!username || !email || !password || username === '' || password  === '' || password === ''){
+        return res.status(400).json({message: 'Please fill in all fields.'})
+    }
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+    const newUser = new User({ username , email , password: hashedPassword })
+
+    try {
+        await newUser.save();
+        res.json('Signup succesfull')
+    } catch (error) {
+        return res.status(400).json('Error ')
+    }  
+}
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config()
 
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGO).then(
     err => console.log(err)
 )
 const app = express()
+app.use(express.json())
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
@@ -18,3 +20,5 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes) // pokud chceme pouzit tu api skrz nejakou route tak uz to neni get jako v tech routes
 // ale uz ji jen pouzivame skrz USE
+
+app.use('/api/auth', authRoutes)
