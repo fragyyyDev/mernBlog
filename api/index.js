9d8c394bdc6903729cd14d5b694e3aa10c762bac
fragyyyDeveloper <fragyyyDev@gmail.com>
2024-09-22 12:52:14 +0200

    Created a test API route with controllers.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
 
 dotenv.config()
 
@@ -14,3 +15,6 @@ const app = express()
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
+
+app.use('/api/user', userRoutes) // pokud chceme pouzit tu api skrz nejakou route tak uz to neni get jako v tech routes
+// ale uz ji jen pouzivame skrz USE
